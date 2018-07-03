1ab8540f88de7a788df3c797b0be8dd5129b2a7f
radoikoff <radoikoff@gmail.com>
2018-07-04 00:38:38 +0300

    App project created with all entities.

---
+++ MiniORM.App/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace MiniORM.App
+{
+    using MiniORM.App.Data;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            const string connectionString = "Server=.;DataBase=MiniORM;Integrated Security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            var emploees = context.Employees.ToList();
+        }
+    }
+}
